375b2b70143f669863340dac2ba0984d625bd549
Faizullin <osmanfajzulla@gmail.com>
2025-08-26 18:27:44 +0500

    tmp-wrofkdlow

--- backend/lms/apps/attachments/urls.py
+++
@@ -1,7 +0,0 @@
-urlpatterns = [
-    # path(
-    #     "api/v1/lms/attachments/",
-    #     api_views.AttachmentListAPIView.as_view(),
-    #     name="attachments-list-api",
-    # ),
-]
--- backend/lms/apps/core/utils/exceptions.py
+++
@@ -1,9 +0,0 @@
-from drf_standardized_errors.handler import exception_handler as standardized_exception_handler
-
-
-class StandardizedViewMixin:
-    def handle_exception(self, exc):
-        response = standardized_exception_handler(exc, self.get_exception_handler_context())
-        if response is None:
-            return super().handle_exception(exc)
-        return response
