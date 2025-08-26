21e32b8551e644b637c28ce9a60dfd5b7bd8c326
Faizullin <osmanfajzulla@gmail.com>
2025-08-26 19:07:05 +0500

    tmp-wrofkdlow

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -10,36 +10,36 @@ then
     echo "PostgreSQL started"
 fi
 
-if [ "$RUN_MIGRATIONS" = 1 ]
-then
-    echo "Running migrations..."
-    python manage.py makemigrations
-    python manage.py collectstatic --no-input --clear
-    echo "Migrations complete"
-fi
+# if [ "$RUN_MIGRATIONS" = 1 ]
+# then
+#     echo "Running migrations..."
+#     python manage.py makemigrations
+#     python manage.py collectstatic --no-input --clear
+#     echo "Migrations complete"
+# fi
 
 
-if [ "$RUN_COLLECTSTATIC" = 1 ]
-then
-    echo "Collecting statics..."
-    python manage.py collectstatic --no-input --clear
-    echo "Collecting statics complete"
-fi
+# if [ "$RUN_COLLECTSTATIC" = 1 ]
+# then
+#     echo "Collecting statics..."
+#     python manage.py collectstatic --no-input --clear
+#     echo "Collecting statics complete"
+# fi
 
 
-if [ "$RUN_MIGRATE" = 1 ]
-then
-    echo "Running migrate..."
-    python manage.py migrate
-    echo "Migrate complete"
-fi
+# if [ "$RUN_MIGRATE" = 1 ]
+# then
+#     echo "Running migrate..."
+#     python manage.py migrate
+#     echo "Migrate complete"
+# fi
 
-if [ "$FLUSH_DATABASE" = 1 ]
-then
-    echo "Flushing database..."
-    python manage.py flush --no-input
-    echo "Database flushed"
-fi
+# if [ "$FLUSH_DATABASE" = 1 ]
+# then
+#     echo "Flushing database..."
+#     python manage.py flush --no-input
+#     echo "Database flushed"
+# fi
 
 
 exec "$@"
