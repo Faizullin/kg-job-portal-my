09487e28e79564afdaa509421a48541724fdfedd
Faiullin <osmanfajzulla@gmail.com>
2025-10-09 00:04:43 +0500

    save

--- backend/scripts/seed.py
+++ backend/scripts/seed.py
@@ -10,7 +10,7 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.dev')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.dev')#
 
 import django
 django.setup()
